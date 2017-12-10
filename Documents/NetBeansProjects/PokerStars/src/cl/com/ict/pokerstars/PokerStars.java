64f8b27d819d8c79be0d980248a475953619a6f7
Oscar Gutiérrez <Oscar Gutirrez@LAPTOP-F6N7DGHV>
2017-12-10 11:36:48 -0300

    Se modifica el main

--- Documents/NetBeansProjects/PokerStars/src/cl/com/ict/pokerstars/PokerStars.java
+++ Documents/NetBeansProjects/PokerStars/src/cl/com/ict/pokerstars/PokerStars.java
@@ -21,7 +21,7 @@ public class PokerStars {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner scanIn =new Scanner(System.in);
+        Scanner scanIn = new Scanner(System.in);
         String inputString = null;
         System.out.println("POKER STARS");
         System.out.println("¿Desea comenzar una partida? (S-s para Si | N-n para No): ");
@@ -35,51 +35,48 @@ public class PokerStars {
 
         }
         while (respuesta.toUpperCase().compareTo("S") == 0) {
-            // creates a console object
-            if (scanIn != null) {
-                // read line from the user input
-                System.out.println("Ingrese el n\u00famero de jugadores (Debe ser menor o igual a 10 y mayor a 1): ");
-                inputString = scanIn.nextLine();
-                int N = 0;
-                while (true) {
-                    try {
-                        N = Integer.parseInt(inputString);
-                        if (N >= 10) {
-                            System.out.println("Ingrese el n\u00famero de jugadores (Debe ser menor o igual a 10): ");
-                            inputString = scanIn.nextLine();
-                            continue;
-                        }
-                        if (N == 1) {
-                            System.out.println("Ingrese el n\u00famero de jugadores (Debe ser mayor a 1): ");
-                            inputString = scanIn.nextLine();
-                            continue;
-                        }
-                        break;
-                    } catch (NumberFormatException e) {
-                        System.out.println("Ingrese un n\u00famero de jugadores v\u00e1lido (Debe ser menor o igual a 10 y mayor a 1): ");
+            System.out.println("Ingrese el n\u00famero de jugadores (Debe ser menor o igual a 10 y mayor a 1): ");
+            inputString = scanIn.nextLine();
+            int N = 0;
+            while (true) {
+                try {
+                    N = Integer.parseInt(inputString);
+                    if (N >= 10) {
+                        System.out.println("Ingrese el n\u00famero de jugadores (Debe ser menor o igual a 10): ");
                         inputString = scanIn.nextLine();
                         continue;
+                    }
+                    if (N == 1) {
+                        System.out.println("Ingrese el n\u00famero de jugadores (Debe ser mayor a 1): ");
+                        inputString = scanIn.nextLine();
+                        continue;
+                    }
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("Ingrese un n\u00famero de jugadores v\u00e1lido (Debe ser menor o igual a 10 y mayor a 1): ");
+                    inputString = scanIn.nextLine();
+                    continue;
 
-                    }
                 }
-                Juego juego = new Juego();
-                Baraja baraja = new Baraja();
-                juego.setBaraja(baraja);
-                juego.setNumeroJugadores(N);
-                juego.generarJugadores();
-                juego.repartirCartas();
-                juego.verificarJugadas();
-                juego.buscarGanador();
-                for (Jugador j : juego.getJugadores()) {
-                    System.out.println("El jugador " + j.getNombre());
-                    System.out.println("Obtuvo la siguiente mano: ");
-                    for (Carta carta : j.getMano()) {
-                        System.out.println(baraja.muestrarInfoCartas(carta));
-                    }
-                }
-                System.out.println("El jugador gandaor fu\u00e9: " + juego.getGanador().getNombre());
-                System.out.println("Y gan\u00f3 con la jugada: " + juego.getJugadaGanadora().getNombre());
             }
+            Juego juego = new Juego();
+            Baraja baraja = new Baraja();
+            juego.setBaraja(baraja);
+            juego.setNumeroJugadores(N);
+            juego.generarJugadores();
+            juego.repartirCartas();
+            juego.verificarJugadas();
+            juego.buscarGanador();
+            for (Jugador j : juego.getJugadores()) {
+                System.out.println("El jugador " + j.getNombre());
+                System.out.println("Obtuvo la siguiente mano: ");
+                for (Carta carta : j.getMano()) {
+                    System.out.println(baraja.muestrarInfoCartas(carta));
+                }
+            }
+            System.out.println("El jugador gandaor fu\u00e9: " + juego.getGanador().getNombre());
+            System.out.println("Y gan\u00f3 con la jugada: " + juego.getJugadaGanadora().getNombre());
+
             System.out.println("¿Desea jugar otra partida? (S-s para Si | N-n para No): ");
             respuesta = scanIn.nextLine();
             while (true) {
